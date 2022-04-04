f9dfb1aa803fa456393bf67fd150346d6c100601
alexesmarie27 <ampress727@gmail.com>
2022-04-04 16:49:45 -0500

    Add Update Functionality

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -9,10 +9,17 @@ export default class NotesAPI {
 
   static saveNote(noteToSave) {
     const notes = NotesAPI.getAllNotes();
+    const existing = notes.find((note) => note.id == noteToSave.id);
 
-    noteToSave.id = Math.floor(Math.random() * 1000000);
-    noteToSave.updated = new Date().toISOString();
-    notes.push(noteToSave);
+    if (existing) {
+      existing.title = noteToSave.title;
+      existing.updated = new Date().toISOString();
+      existing.body = noteToSave.body;
+    } else {
+      noteToSave.id = Math.floor(Math.random() * 1000000);
+      noteToSave.updated = new Date().toISOString();
+      notes.push(noteToSave);
+    }
 
     localStorage.setItem("notesapp-notes", JSON.stringify(notes));
   }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import NotesAPI from "./NotesAPI.js";
 
-// NotesAPI.saveNote({
-//   id: 881570,
-//   title: "the title has changed",
-//   body: "I am a new note",
-// });
+NotesAPI.saveNote({
+  id: 881570,
+  title: "the title has changed",
+  body: "I am a new note",
+});
 
 console.log(NotesAPI.getAllNotes());

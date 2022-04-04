d914f2cd70c876c4244c9629a8a6571b56de1007
alexesmarie27 <ampress727@gmail.com>
2022-04-04 16:45:46 -0500

    Add Save Note Functionality

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -1,7 +1,21 @@
 export default class NotesAPI {
-  static getAllNotes() {}
+  static getAllNotes() {
+    const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
 
-  static saveNote(noteToSave) {}
+    return notes.sort((a, b) => {
+      return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+    });
+  }
+
+  static saveNote(noteToSave) {
+    const notes = NotesAPI.getAllNotes();
+
+    noteToSave.id = Math.floor(Math.random() * 1000000);
+    noteToSave.updated = new Date().toISOString();
+    notes.push(noteToSave);
+
+    localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+  }
 
   static deleteNote(id) {}
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 import NotesAPI from "./NotesAPI.js";
+
+// NotesAPI.saveNote({
+//   id: 881570,
+//   title: "the title has changed",
+//   body: "I am a new note",
+// });
+
+console.log(NotesAPI.getAllNotes());

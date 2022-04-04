8bec13426522fc56f6ae422160da8289a79c09cc
alexesmarie27 <ampress727@gmail.com>
2022-04-04 18:16:14 -0500

    Create App.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-import NotesView from "./NotesView.js";
-import NotesAPI from "./NotesAPI.js";
+import App from "./App.js";
 
-const app = document.getElementById("app");
-const view = new NotesView(app, {
-  onNoteAdd() {
-    console.log("Let's add a new note");
-  },
-  onNoteEdit(newTitle, newBody) {
-    console.log(newTitle);
-    console.log(newBody);
-  },
-  onNoteSelect(id) {
-    console.log("Note selected: " + id);
-  },
-  onNoteDelete(id) {
-    console.log("Note deleted: " + id);
-  },
-});
-
-const notes = NotesAPI.getAllNotes();
-
-view.updateNotesList(notes);
-view.updateActiveNote(notes[0]);
+const root = document.getElementById("app");
+const app = new App(root);
